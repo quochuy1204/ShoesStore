5c48d6ef1a6d7d8bac406fb768c8ba2fef8788c0
Dang Trong Quoc Huy <quochuy1204>
2020-11-19 22:02:48 +0700

    Modified app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is an demo for little shoes store website.
+          Quoc Huy
         </p>
         <a
           className="App-link"
