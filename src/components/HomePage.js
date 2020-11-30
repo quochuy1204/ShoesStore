95dfc7656a99d0a1e02197d010ca3dedbe246df3
Dang Trong Quoc Huy <quochuy1204>
2020-11-30 10:01:58 +0700

    Update 30/11/2020

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className='HomePage'>
+                <h1>Hello this is my first component.</h1>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
